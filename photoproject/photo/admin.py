6c2fc4bbefd569dc466393703661aa0ba5ed9975
fko2547015 <fko2547015@stu.o-hara.ac.jp>
2025-11-19 14:21:25 +0900

    変更

--- photoproject/photo/admin.py
+++ photoproject/photo/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Categoty, PhotoPost
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id','title')
+    list_display_links = ('id','title')
+
+class PhotoPostAdmin(admin.ModelAdmin):
+    list_display = ('id','title')
+    list_display_links = ('id','title')
+
+admin.site.register(Categoty, CategoryAdmin)
+admin.site.register(PhotoPost, PhotoPostAdmin)
--- photoproject/photoproject/settings.py
+++ photoproject/photoproject/settings.py
@@ -126,3 +126,15 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+
+DEFAULT_FROM_EMAIL ='naoichigo.1310@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'naoichigo.1310@gmail.com'
+EMAIL_HOST_PASSWORD = 'mngx krtr sxcv mkmo'
+EMAIL_USE_TLS = True
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
