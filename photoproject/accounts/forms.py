e4c8328e9cf69ca605d56a9354921c23ce92a9d7
fko2547015 <fko2547015@stu.o-hara.ac.jp>
2025-11-19 10:40:44 +0900

    変更

--- photoproject/accounts/forms.py
+++ photoproject/accounts/forms.py
@@ -2,5 +2,6 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
-    model = CustomUser
-    fields = ('username', 'email', 'password1', 'password2')
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'password1', 'password2')
