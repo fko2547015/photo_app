24437c490b3bee2744f5b3e3f0a083cde98e52fe
fko2547015 <fko2547015@stu.o-hara.ac.jp>
2025-11-19 10:26:12 +0900

    変更

--- photoproject/accounts/urls.py
+++ photoproject/accounts/urls.py
@@ -7,6 +7,6 @@ app_name = 'accounts'
 urlpatterns = [
     path('signup/',views.SignUpView.as_view(),name='signup'),
     path('signup_success/',views.SignUpSuccessView.as_view(),name='signup_success'),
-    path('login/',auth_view.LoginView.as_view(template_name='login.html'),),
-    path('logout/',auth_view.LogoutView.as_view(template_name='logout.html'),),
+    path('login/',auth_view.LoginView.as_view(template_name='login.html'),name='login'),
+    path('logout/',auth_view.LogoutView.as_view(template_name='logout.html'),name='logout'),
 ]
--- photoproject/photoproject/urls.py
+++ photoproject/photoproject/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('photo.urls')),
-    path('', include('accounts.urls'),)
+    path('', include('accounts.urls')),
+    # path('password_reset/',auth_views.PasswordResetView.as_view(template_name="password_reset.html"),name='password_reset'),
+    # path('password_reset/done/',auth_views.PasswordChangeDoneView.as_view(template_name="password_reset_sent.html"),name='password_reset_done'),
+    # path('reset/<uidb64>/<token>',auth_views.PasswordResetConfirmView.as_view())
 ]
